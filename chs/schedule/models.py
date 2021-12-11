8e9c6f13ba9311d676f7680d5f67fbba607ad145
Ubuntu <ubuntu@ip-172-31-13-196.ap-northeast-2.compute.internal>
2021-12-11 10:21:05 +0000

    second commit

--- chs/schedule/models.py
+++ chs/schedule/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Schedule(models.Model):
+    title = models.CharField(max_length = 100)
+    '''
+    type = 
+    start_date = 
+    end_date =
+    start_time =
+    end_time = 
+    status =
+    '''
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
